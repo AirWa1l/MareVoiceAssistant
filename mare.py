8285c294bb49b097db61522f89f3301990643b57
Jose Adrian Marin <jose.adrian.marin@correounivalle.edu.co>
2026-01-07 11:16:21 -0500

    Added time and datefunction, and some commentaries about how pyttsx3 works

--- mare.py
+++ mare.py
@@ -1,5 +1,8 @@
-import speech_recognition as sr
+# import speech_recognition as sr
 import pyttsx3
+import datetime
+
+# Para hacer varias ejecuciones se debe iniciarlizar el motor en cada iteración para evitar atascos
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -10,5 +13,23 @@ engine.setProperty('rate', newVoiceRate)
 def speech(audio):
     engine.say(audio)
     engine.runAndWait()
+    engine.stop()
 
-speech("Hola, como estas")
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    return Time
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    day = int(datetime.datetime.now().day)
+    date_time = f"{year}, {month} and {day}"
+    # date_time = [year, month, day] es preferible no pasar listas, pyttsx3 procesa solo texto, por eso se daba ese error
+    return date_time
+
+# Por alguna razón esto no funciona como debería, pues pronuncia todo de forma incompleta
+# adicionalmente, el speaker habla usando el idioma del computador
+
+date()
+test = f"{date()}, {time()} y hola, como estas?"
+speech(test)
